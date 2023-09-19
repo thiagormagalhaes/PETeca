94bf165c6c21470fbe36effc1a1418e908fa3bfd
Thiago Magalhaães <thiagoelmagalhaes@gmail.com>
2023-09-19 08:51:43 -0300

    create context

---
+++ PETeca.Infra/Data/PETecaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PETeca.Infra.Data
+{
+    public class PETecaContext : DbContext
+    {
+        public PETecaContext(DbContextOptions<PETecaContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var mappingsAssemply = GetType().Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(mappingsAssemply);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
