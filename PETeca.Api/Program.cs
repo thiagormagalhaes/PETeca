af2baca994954182802e40d4455420cb6656df4b
Thiago Magalhaães <thiagoelmagalhaes@gmail.com>
2023-09-25 19:13:15 -0300

    add entity framework

--- PETeca.Api/Program.cs
+++ PETeca.Api/Program.cs
@@ -1,20 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PETeca.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", false, true);
+builder.Configuration.AddJsonFile("ef-configuration.json", false, true);
+
+builder.Services.AddDbContext<PETecaContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<PETecaContext>();
+    dataContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
